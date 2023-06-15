78ad07075b701503b9ad6ef84fce7624dad4fe78
kang-on-working <kang.onworking@gmail>
2023-06-15 16:46:53 +0900

    User Custom Key

--- Crypto/main.go
+++ Crypto/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-//	"bufio"
-//	"os"
+	"bufio"
+	"os"
 	"fmt"
 	"pkg"
 )
 
 func main() {
-	key := "My Example Key"
+
+	var pnt *string
+	if len(os.Args) < 2 {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter Encryption key: ")
+		key, _ := reader.ReadString('\n')
+		key = key[:len(key)-1] // 개행 문자 제거
+		pnt = &key	
+
+	} else { 
+		var param string = os.Args[1]
+		pnt = &param
+	}
+
+	key := *pnt
 
 	git, err := pkg.JsonParse("./git.json")	
 	if err != nil {
@@ -33,33 +47,4 @@ func main() {
 
 	fmt.Println(DecryptedGit)
 
-	/*
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter plain text: ")
-	plainText, _ := reader.ReadString('\n')
-	plainText = plainText[:len(plainText)-1] // 개행 문자 제거
-
-	fmt.Print("Enter Encryption key: ")
-	key, _ := reader.ReadString('\n')
-	key = key[:len(key)-1] // 개행 문자 제거
-
-
-	EncryptedText, err := pkg.EncryptStr(plainText, key)
-	if err != nil {
-		fmt.Println("Error Encrypting text:", err)
-		return
-	}
-
-	fmt.Println("Encrypted text:", EncryptedText)
-
-	DecryptedText, err := pkg.DecryptStr(EncryptedText, key)
-	if err != nil {
-		fmt.Println("Error Decrypting text:", err)
-		return
-	}
-
-	fmt.Println("Decrypted text:", DecryptedText)
-
-	*/
 }
