4d1730638b6514569a30eca1aa33f1c2c03ce7be
kang-on-working <kang.onworking@gmail>
2023-06-15 16:33:08 +0900

    Json En/Decyrpting

--- Crypto/main.go
+++ Crypto/main.go
@@ -8,12 +8,30 @@ import (
 )
 
 func main() {
+	key := "My Example Key"
+
 	git, err := pkg.JsonParse("./git.json")	
 	if err != nil {
 		fmt.Println("Json Parsing Error: ", err)
 		return
 	}
 	fmt.Println(git)
+
+	EncryptedGit, err := pkg.EncryptJsonValue(git, key)
+	if err != nil {
+		fmt.Println("Json Encrypt Error:", err)
+		return
+	}
+
+	fmt.Println(EncryptedGit)
+
+	DecryptedGit, err := pkg.DecryptJsonValue(EncryptedGit, key)
+	if err != nil {
+		fmt.Println("Json Decrypt Error:", err)
+		return
+	}
+
+	fmt.Println(DecryptedGit)
 
 	/*
 	reader := bufio.NewReader(os.Stdin)
@@ -29,19 +47,19 @@ func main() {
 
 	EncryptedText, err := pkg.EncryptStr(plainText, key)
 	if err != nil {
-		fmt.Println("Error EncryptString text:", err)
+		fmt.Println("Error Encrypting text:", err)
 		return
 	}
 
-	fmt.Println("EncryptStred text:", EncryptedText)
+	fmt.Println("Encrypted text:", EncryptedText)
 
 	DecryptedText, err := pkg.DecryptStr(EncryptedText, key)
 	if err != nil {
-		fmt.Println("Error DecryptString text:", err)
+		fmt.Println("Error Decrypting text:", err)
 		return
 	}
 
-	fmt.Println("DecryptStred text:", DecryptedText)
+	fmt.Println("Decrypted text:", DecryptedText)
 
 	*/
 }
--- Crypto/pkg/crypto.go
+++ Crypto/pkg/crypto.go
@@ -8,7 +8,6 @@ import (
 	"errors"
 	"crypto/rand"
 	"io"
-
 )
 
 func keyToHash(key string) []byte {
@@ -18,12 +17,12 @@ func keyToHash(key string) []byte {
 
 // Decrypt 함수는 주어진 암호화된 문자열을 복호화하여 원래 문자열을 반환합니다.
 func DecryptStr(encryptedStr string, key string) (string, error) {
+	HashKey := keyToHash(key)
 
 	ciphertextBytes, err := base64.StdEncoding.DecodeString(encryptedStr)
 	if err != nil {
 		return "", err
 	}
-	HashKey := keyToHash(key)
 	block, err := aes.NewCipher(HashKey)
 	if err != nil {
 		return "", err
@@ -69,5 +68,64 @@ func EncryptStr(plainText string, key string) (string, error) {
 
 	cipherText := gcm.Seal(nonce, nonce, []byte(plainText), nil)
 	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+// EncryptJsonValue 함수는 주어진 git 구조체의 각각의 필드 값을 암호화하여 새로운 git_encrypted 구조체를 반환합니다.
+func EncryptJsonValue(git GIT, key string) (git_encrypted GIT, err error) {
+	encryptedEmail, err := EncryptStr(git.Email, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	encryptedUsername, err := EncryptStr(git.Username, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	encryptedToken, err := EncryptStr(git.Token, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	encryptedRepo, err := EncryptStr(git.Repo, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	return GIT{
+		Email:    encryptedEmail,
+		Username: encryptedUsername,
+		Token:    encryptedToken,
+		Repo:     encryptedRepo,
+	}, nil
+}
+
+func DecryptJsonValue(git GIT, key string) (git_decrypted GIT, err error) {
+	decryptedEmail, err := DecryptStr(git.Email, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	decryptedUsername, err := DecryptStr(git.Username, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	decryptedToken, err := DecryptStr(git.Token, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	decryptedRepo, err := DecryptStr(git.Repo, key)
+	if err != nil {
+		return GIT{}, err
+	}
+
+	return GIT{
+		Email:    decryptedEmail,
+		Username: decryptedUsername,
+		Token:    decryptedToken,
+		Repo:     decryptedRepo,
+	}, nil
 }
 
