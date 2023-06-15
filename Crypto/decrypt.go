2ca5122d879c21b371d6e7b902e3dd940b1531e4
kang-on-working <kang.onworking@gmail>
2023-06-15 17:15:19 +0900

    Feature : Json File Output

---
+++ Crypto/decrypt.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"pkg"
+)
+
+func main() {
+
+	JsonFile := "./git.json"
+
+	var pnt *string
+	if len(os.Args) < 2 {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter Encryption key: ")
+		key, _ := reader.ReadString('\n')
+		key = key[:len(key)-1] // 개행 문자 제거
+		pnt = &key	
+
+	} else { 
+		var param string = os.Args[1]
+		pnt = &param
+	}
+
+	key := *pnt
+
+	git, err := pkg.JsonParse(JsonFile)	
+	if err != nil {
+		fmt.Println("Json Parsing Error: ", err)
+		return
+	}
+	fmt.Println(git)
+
+	DecryptedGit, err := pkg.DecryptJsonValue(git, key)
+	if err != nil {
+		fmt.Println("Json Decrypt Error:", err)
+		return
+	}
+	JsonData, err := pkg.GitToJson(DecryptedGit)
+	
+	err = ioutil.WriteFile(JsonFile, JsonData, 0644)
+
+	fmt.Println(DecryptedGit)
+
+}
