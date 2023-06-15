1d2878b571b9d85037aacf1cad1799aed3c20be3
kang-on-working <kang.onworking@gmail>
2023-06-15 16:54:30 +0900

    Sep crypt pkg to use again

--- Crypto/pkg/decrypt.go
+++ Crypto/pkg/decrypt.go
@@ -5,9 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/sha256"
 	"encoding/base64"
-	"errors"
-	"crypto/rand"
-	"io"
+	"fmt"
 )
 
 func keyToHash(key string) []byte {
@@ -35,7 +33,7 @@ func DecryptStr(encryptedStr string, key string) (string, error) {
 
 	nonceSize := gcm.NonceSize()
 	if len(ciphertextBytes) < nonceSize {
-		return "", errors.New("cipherText too short")
+		return "", fmt.Errorf("cipherText too short")
 	}
 
 	nonce, ciphertext := ciphertextBytes[:nonceSize], ciphertextBytes[nonceSize:]
@@ -48,57 +46,6 @@ func DecryptStr(encryptedStr string, key string) (string, error) {
 }
 
 
-// Encrypt 함수는 주어진 원문 문자열을 암호화하여 암호화된 문자열을 반환합니다.
-func EncryptStr(plainText string, key string) (string, error) {
-	HashKey := keyToHash(key)
-	block, err := aes.NewCipher(HashKey)
-	if err != nil {
-		return "", err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", err
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", err
-	}
-
-	cipherText := gcm.Seal(nonce, nonce, []byte(plainText), nil)
-	return base64.StdEncoding.EncodeToString(cipherText), nil
-}
-
-// EncryptJsonValue 함수는 주어진 git 구조체의 각각의 필드 값을 암호화하여 새로운 git_encrypted 구조체를 반환합니다.
-func EncryptJsonValue(git GIT, key string) (git_encrypted GIT, err error) {
-	encryptedEmail, err := EncryptStr(git.Email, key)
-	if err != nil {
-		return GIT{}, err
-	}
-
-	encryptedUsername, err := EncryptStr(git.Username, key)
-	if err != nil {
-		return GIT{}, err
-	}
-
-	encryptedToken, err := EncryptStr(git.Token, key)
-	if err != nil {
-		return GIT{}, err
-	}
-
-	encryptedRepo, err := EncryptStr(git.Repo, key)
-	if err != nil {
-		return GIT{}, err
-	}
-
-	return GIT{
-		Email:    encryptedEmail,
-		Username: encryptedUsername,
-		Token:    encryptedToken,
-		Repo:     encryptedRepo,
-	}, nil
-}
 
 func DecryptJsonValue(git GIT, key string) (git_decrypted GIT, err error) {
 	decryptedEmail, err := DecryptStr(git.Email, key)
@@ -128,4 +75,3 @@ func DecryptJsonValue(git GIT, key string) (git_decrypted GIT, err error) {
 		Repo:     decryptedRepo,
 	}, nil
 }
-
